5ab84a942a142ed3b5a68c372a93e82cf1430d4e
Ринат Давляшов <rinat_rinat@MacBook-Air-Rinat.local>
2021-03-21 16:10:17 +0300

    update function

--- lesson/угадай число (с уловиями).py
+++ lesson/угадай число (с уловиями).py
@@ -1,16 +1,20 @@
-num = int(input()) # ввод в программу условий данный под num-число
 
-if num < 100:
-    print("Меньше 100")
-    if num%2==0:
-        print("Делится на 2")
-    elif num%3==0:
-        print("Делится на 3")
-elif num > 100:
-    print("Больше 100")
-    if num%2==0:
-        print("Делится на 2")
-    elif num%3==0:
-        print("Делится на 3")
-else:
-    print("что-то")
+#num = int(input()) # ввод в программу условий данный под num-число
+def find_number_in_range(num):
+    if num < 100:
+        print("Меньше 100")
+        if num%2==0:
+            print("Делится на 2")
+        elif num%3==0:
+            print("Делится на 3")
+    elif num > 100:
+        print("Больше 100")
+        if num%2==0:
+            print("Делится на 2")
+        elif num%3==0:
+            print("Делится на 3")
+    else:
+        print("Вы угадали предел")
+
+
+find_number_in_range(int(input("Введите число: ")))
